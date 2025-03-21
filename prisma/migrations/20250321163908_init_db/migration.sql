cd33a9e3ec8856e50152eee3defaec7631572f0a
levg34 <levg34@users.noreply.github.com>
2025-03-21 16:51:47 +0000

    init prisma and DB

---
+++ prisma/migrations/20250321163908_init_db/migration.sql
@@ -0,0 +1,21 @@
+-- CreateEnum
+CREATE TYPE "Difficulty" AS ENUM ('Facile', 'Moyen', 'Difficile', 'TresDifficile');
+
+-- CreateEnum
+CREATE TYPE "Category" AS ENUM ('Pedagogie', 'Arrangement', 'Pastiches', 'OeuvresOriginales');
+
+-- CreateTable
+CREATE TABLE "Score" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "instrument" TEXT NOT NULL,
+    "price" TEXT NOT NULL,
+    "image" TEXT,
+    "pdfUrl" TEXT,
+    "audioUrl" TEXT,
+    "difficulty" "Difficulty" NOT NULL,
+    "category" "Category" NOT NULL,
+    "usedInstruments" TEXT[],
+
+    CONSTRAINT "Score_pkey" PRIMARY KEY ("id")
+);
